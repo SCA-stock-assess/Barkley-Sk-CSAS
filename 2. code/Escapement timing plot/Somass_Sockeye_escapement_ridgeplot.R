e0ce5c591663344f88f4b037c3e8bf55b0225ae8
Nick Brown <nickawbrown@gmail.com>
2024-12-31 10:49:44 -0800

    try using daily proportion values directly to draw ridgelines

--- 2. code/Escapement timing plot/Somass_Sockeye_escapement_ridgeplot.R
+++ 2. code/Escapement timing plot/Somass_Sockeye_escapement_ridgeplot.R
@@ -1,19 +1,43 @@
 # Packages ----------------------------------------------------------------
 
-pkgs <- c("tidyverse", "here", "readxl", "ggridges")
+pkgs <- c("tidyverse", "here", "readxl", "ggridges", "janitor")
 #install.packages(pkgs)
 
-library(tidyverse); theme_set(theme_ridges())
+
 library(here)
 library(readxl)
 library(ggridges)
+library(tidyverse); theme_set(theme_ridges())
 
 
 # Load historical Sockeye escapement data -----------------------------
 
 
-# Escapement data
-escday <- read_xlsx(
+# Somass escapement data (historic 1943-1992)
+som_hist <- read_xls(
+  here(
+    "1. data",
+    "1943-1992 GCLESCWL.xls"
+  ),
+  sheet = "GCLESCWL"
+) |> 
+  janitor::clean_names() |> 
+  mutate(
+    year = as.integer(format(date, "%Y")),
+    d_m = paste0(
+      format(date, "%d"),
+      "-",
+      format(date, "%b")
+    ),
+    system = "GCL",
+    method = "fence count"
+  ) |> 
+  rename("sockeye" = gc_sockeye) |> 
+  select(year, d_m, sockeye, system, method)
+
+
+# Somass escapement data (1975+)
+som_esc <- read_xlsx(
   here(
     "1. data",
     "Somass_Sockeye_daily_escapement_data.xlsx"
@@ -46,23 +70,61 @@ escday <- read_xlsx(
       \(x) if_else(x < 0, 0, x)
     )
   ) |> 
-  select(month, day, d_m, system, year, contains("adj_")) |> 
+  select(d_m, system, year, contains("adj_")) |> 
   rowwise() |> 
-  mutate(sockeye = sum(c_across(c(adj_adults, adj_jacks)), na.rm = TRUE)) 
+  mutate(
+    sockeye = sum(c_across(c(adj_adults, adj_jacks)), na.rm = TRUE),
+    method = "video analysis",
+    .keep = "unused"
+  ) 
 
 
+# Hucuktlis data
+huc_esc <- read_xlsx(
+  here(
+    "1. data",
+    "HucuktlisRiver_fence-counts_1997-2024.xlsx"
+  )
+) |> 
+  pivot_longer(
+    matches("\\d{4}"),
+    names_to = "year",
+    values_to = "sockeye"
+  ) |> 
+  mutate(
+    d_m = paste0(
+      format(Date, "%d"),
+      "-",
+      format(Date, "%b")
+    ),
+    year = as.integer(year),
+    system = "HUC",
+    method = "fence count",
+    .keep = "unused"
+  )
 
-# Ridgeline plot ---------------------------------------------------------
+
+# Collate all escapement data
+escday <- som_hist |> 
+  filter(year < min(som_esc$year)) |> 
+  bind_rows(
+    huc_esc,
+    som_esc
+  ) |> 
+  mutate(system = factor(system, levels = c("GCL", "SPR", "HUC")))
 
 
-# Plot for current year
-(ridge_p <- escday |> 
-   select(year, d_m, system, sockeye) |> 
+# Ridgeline plots --------------------------------------------------------
+
+
+# Plot using calculated stats
+base_p1 <- escday |> 
+   filter(!is.na(sockeye)) |> 
    mutate(sockeye = round(sockeye, 0)) |> 
    uncount(sockeye) |> 
    # Subsample rows down to 500000 random draws
    # Useful when toying with plot parameters
-   # slice_sample(n = 5e5) |> # deactivate when saving full version of plot
+   slice_sample(n = 5e5) |> # deactivate when saving full version of plot
    ggplot(
      aes(
        y = year, 
@@ -70,47 +132,105 @@ escday <- read_xlsx(
        group = year
      )
    ) +
-   facet_wrap(
-     ~system,
-     labeller = labeller(
-       system = c(
-         "GCL" = "Stamp Falls fishway",
-         "SPR" = "Sproat Falls fishway"
-       )
-     )
-   ) +
    geom_density_ridges(
+     aes(fill = method, colour = method),
      quantile_lines = TRUE,
      quantiles = 2,
+     alpha = 0.6,
      vline_colour = "red",
-     #rel_min_height = 0.001
-   ) +
-   scale_x_date(expand = c(0, 0)) +
-   scale_y_reverse(expand = c(0, 0)) +
-   coord_cartesian(clip = "off") +
-   labs(
-     x = "Date", 
-     y = "Adult return year",
-     title = "Sockeye counts through Somass system fishways"
-   ) +
-   theme(
-     strip.background = element_blank(),
-     strip.text = element_text(margin = margin(t = 0.5, b = 0.3, unit = "lines"))
-   )
-)
+     rel_min_height = 1e-4
+   ) 
 
 
-# Save the plot
-ggsave(
-  ridge_p,
-  filename = here(
-    "3. outputs",
-    "Plots",
-    "Somass_Sockeye_escapement_ridgelines.png"
-  ),
-  width = 6.5,
-  height = 8,
-  units = "in",
-  dpi = "print"
-)
+# Ridgeline plot where values are mapped to curve height directly
+base_p2 <- escday |> 
+    mutate(
+      .by = c(system, year),
+      ttl = sum(sockeye, na.rm = TRUE),
+      prop = sockeye/ttl
+    ) |> 
+    ggplot(
+      aes(
+        y = year,
+        x = as.Date(d_m, format = "%d-%b"),
+        height = prop,
+        group = year
+      )
+    ) +
+    geom_density_ridges(
+      stat = "identity",
+      aes(fill = method, colour = method),
+      alpha = 0.6,
+      scale = 1.5
+    ) 
 
+
+# Apply consistent formatting to both plots
+ridge_plots <- list(
+  "density_ridges" = base_p1, 
+  "proportion_ridges" = base_p2
+) |> 
+  map(
+    \(x) x +
+      facet_wrap(
+        ~system,
+        labeller = labeller(
+          system = c(
+            "GCL" = "Stamp Falls fishway",
+            "SPR" = "Sproat Falls fishway",
+            "HUC" = "Hucuktlis River fence"
+          )
+        )
+      ) +
+      scale_x_date(
+        expand = c(0, 0), 
+        date_breaks = "1 month",
+        date_labels = "%b"
+      ) +
+      scale_y_reverse(
+        breaks = scales::breaks_pretty(n = 10),
+        expand = c(0, 0)
+      ) +
+      scale_fill_manual(
+        values = c("black", "grey70"),
+        aesthetics = c("fill", "colour"),
+        name = "Counting\nmethod"
+      ) +
+      coord_cartesian(clip = "off") +
+      labs(
+        x = "Date", 
+        y = "Adult return year",
+        title = "Sockeye counts through local enumeration sites"
+      ) +
+      theme(
+        legend.position = "inside",
+        legend.position.inside = c(0.98, 0.98),
+        legend.justification.inside = c(1, 1),
+        legend.box.background = element_rect(colour = "black", fill = alpha("white", 0.4)),
+        strip.background = element_blank(),
+        strip.text = element_text(margin = margin(t = 0.5, b = 0.3, unit = "lines")),
+        axis.text.x = element_text(angle = 45, hjust = 1)
+      )
+  )
+
+
+# Save the plots
+ridge_plots |> 
+  iwalk(
+    \(x, idx) ggsave(
+      x,
+      filename = here(
+        "3. outputs",
+        "Plots",
+        paste0(
+          "Barkley_Sockeye_escapement_",
+          idx,
+          ".png"
+        )
+      ),
+      width = 6.5,
+      height = 6.5,
+      units = "in",
+      dpi = "print"
+    )
+  )
