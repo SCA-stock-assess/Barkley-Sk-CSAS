a6bcc8c3770e1d0028f373a27d502c292032a365
Nick Brown <nickawbrown@gmail.com>
2025-01-23 11:06:51 -0800

    add data source information and work into plot colour palettes

--- 2. code/Population structure plots/abundance_time-series_plotting.R
+++ 2. code/Population structure plots/abundance_time-series_plotting.R
@@ -1,11 +1,12 @@
 # Packages ----------------------------------------------------------------
 
-pkgs <- c("tidyverse", "here", "readxl")
+pkgs <- c("tidyverse", "here", "readxl", "geomtextpath")
 #install.packages(pkgs)
 
 library(tidyverse); theme_set(theme_bw())
 library(here)
 library(readxl)
+library(geomtextpath)
 
 
 
@@ -15,7 +16,7 @@ library(readxl)
 # These data include reconstructed catch data from Henderson based on
 # a relationship to Somass Sockeye harvest rate. To update these data,
 # run the script under 2. code/Stock-recruit data preparation
-abun_data <- here(
+abun_data0 <- here(
   "3. outputs",
   "Total return time series",
   "Barkley Sockeye total annual returns by CU_collated.xlsx"
@@ -32,45 +33,185 @@ abun_data <- here(
   )
 
 
+# Set colour palette for escapement and catch/stockID data
+esc_pal <- "YlGnBu"
+catch_pal <- "Greys"
+
+
+# Create colour palette for escapement data sources
+escapement_data_cols <- abun_data0 |> 
+  mutate(
+    escapement_data_source = factor(
+      escapement_data_source,
+      # Rank data sources by reliability/accuracy
+      # best to worst, descending
+      levels = c(
+        "video counts",
+        "manual transport count",
+        "resistivity counter",
+        "AUC survey - Clemens",
+        "fence count",
+        "hatchery reports",
+        "fishery officer",
+        "partial shoreline estimate"
+      )
+    )
+  ) |> 
+  arrange(desc(escapement_data_source)) |> 
+  distinct(escapement_data_source) |> 
+  drop_na() |> 
+  mutate(
+    .by = escapement_data_source,
+    level = cur_group_id() + 0 # Add integers to level to darken the colours
+  ) |> 
+  mutate(escapement_colour = RColorBrewer::brewer.pal(max(level), esc_pal)[level]) |> 
+  select(-level)
+
+
+# Create colour palette for catch data sources
+catch_data_cols <- abun_data0 |> 
+  # It is really only the cannery reports that are less accurate
+  # for the raw catch data
+  mutate(level = if_else(catch_data_source == "cannery pack", 2, 3)) |> 
+  arrange(desc(level)) |> 
+  distinct(catch_data_source, level) |> 
+  drop_na() |> 
+  mutate(catch_colour = RColorBrewer::brewer.pal(max(level), catch_pal)[level]) |> 
+  select(-level)
+
+
+# Create colour palette for Stock ID sources
+stockid_cols <- abun_data0 |> 
+  mutate(
+    stockid_source = factor(
+      stockid_source,
+      # Rank data sources by reliability/accuracy
+      # best to worst, descending
+      levels = c(
+        "DNA attribution",
+        "escapement",
+        "MLE run timing model",
+        "assumed run timing",
+        "catch location"
+      )
+    )
+  ) |> 
+  arrange(desc(stockid_source)) |> 
+  distinct(stockid_source) |> 
+  drop_na() |> 
+  mutate(
+    .by = stockid_source,
+    level = cur_group_id() + 1 # Add integers to level to darken the colours
+  ) |> 
+  mutate(stockid_colour = RColorBrewer::brewer.pal(max(level), catch_pal)[level]) |> 
+  select(-level)
+
+
+# Assign some colour palettes to data sources for plotting purposes
+abun_data <- abun_data0 |> 
+  left_join(stockid_cols) |> 
+  left_join(escapement_data_cols) |> 
+  left_join(catch_data_cols) |> 
+  mutate(
+    escapement_data_source = factor(
+      escapement_data_source, 
+      levels = levels(escapement_data_cols$escapement_data_source)
+    ),
+    catch_data_source = fct_relevel(catch_data_source, "cannery pack", after = Inf),
+    stockid_source = factor(
+      stockid_source, 
+      levels = levels(stockid_cols$stockid_source)
+    )
+  )
+
+
 
 # Make time series plot for SMU aggregate --------------------------------------
 
 
+# Prepare dataframe with aggregate abundance values only
+agg_abun_data <- abun_data |> 
+  select(
+    year,
+    escapement,
+    annual_Barkley_catch,
+    matches("(catch|escapement)_colour"),
+    matches("(catch|escapement)_data_source")
+  ) |> 
+  distinct(.keep_all = TRUE) |> 
+  # Hacky fix to ensure aggregate Barkley catch is summed correctly within years
+  add_count(year) |> 
+  mutate(catch_value = annual_Barkley_catch/n) |> 
+  # Rename columns to facilitate simpler pivot code
+  rename("escapement_value" = escapement) |> 
+  pivot_longer(
+    matches("(catch|escapement)_.*"),
+    names_pattern = "([[:alpha:]]*)_(.*)",
+    names_to = c("name", ".value")
+  ) |> 
+  # Ensure catch data source information is the same within years
+  group_by(year, name) |> 
+  fill(colour, data_source, .direction = "updown") |> 
+  ungroup() |> 
+  # Ensure levels of data_source and colour appear in order in the plot
+  mutate(
+    data_source = factor(
+      data_source,
+      levels = c(
+        levels(abun_data$catch_data_source), 
+        levels(abun_data$escapement_data_source)
+      ),
+      labels = c(
+        rep("catch records", 3),
+        "cannery pack",
+        levels(abun_data$escapement_data_source)
+      )
+    )
+  ) |> 
+  summarize(
+    .by = c(year, name, colour, data_source),
+    value = sum(value, na.rm = TRUE)
+  ) |> 
+  arrange(data_source) |> 
+  mutate(colour = fct_inorder(colour)) |> 
+  filter(value > 0) #remove the shoreline estimates
+  
+
 # Plot total returns broken out between catch and escapement
-(abun_p <- abun_data |> 
-   pivot_longer(contains("annual")) |> 
-   distinct(year, name, value) |> 
-   mutate(name = str_remove(name, "annual_Barkley_")) |> 
+(abun_p <- agg_abun_data |> 
    ggplot(aes(x = year, y = value/1000)) +
-   geom_vline(
-     xintercept = 1969, 
+   geom_textvline(
+     xintercept = 1968.5, 
      lty = 2, 
-     colour = "grey50"
-   ) +
-   geom_col(aes(fill = name), colour = "black") +
-   annotate(
-     "text",
-     x = 1968,
-     y = 600,
-     label = "Lake Fertilization Program begins",
-     angle = 90,
      colour = "grey50",
-     vjust = 0,
-     hjust = 0
+     label = "Great Central Lake fertilization begins",
+     size = 3,
+     hjust = 0.75
+   ) +
+   geom_col(
+     aes(
+       group = data_source,
+       fill = colour
+     ),
+     position = "stack",
+     colour = "black"
+   ) +
+   scale_fill_identity(
+     name = "Catch (grey) and\nescapement (colour)\ndata sources",
+     guide = "legend",
+     labels = levels(agg_abun_data$data_source)
    ) +
    scale_y_continuous(
      name = "Barkley Sockeye abundance (1000s)",
      labels = scales::label_number(),
      expand = expansion(mult = c(0, 0.05))
    ) +
-   scale_fill_manual(values = c("grey", "black")) +
    labs(x = "Return year") +
    theme(
      legend.position = "inside",
      legend.position.inside = c(0.02, 0.98),
      legend.justification.inside = c(0, 1),
-     legend.background = element_rect(colour = "black", fill = "white"),
-     legend.title = element_blank()
+     legend.background = element_rect(colour = "black", fill = "white")
    )
 )
 
@@ -104,11 +245,42 @@ cu_catch <- tribble(
   mutate(CU = factor(CU, levels = c("Great Central", "Sproat", "Hucuktlis"))) 
 
 
+# Prepare CU-level data for plotting
+cu_abun_data <- abun_data |> 
+  rename_with(.cols = c(catch, escapement), \(x) paste0(x, "_value")) |> 
+  # Use Stock ID information as catch information
+  mutate(
+    catch_data_source = stockid_source,
+    catch_colour = stockid_colour
+  ) |> 
+  filter(!CU == "Somass aggregate") |> 
+  select(
+    year, 
+    CU,
+    matches("(catch|escapement)_(value|data_source|colour)")
+  ) |> 
+  pivot_longer(
+    matches("(catch|escapement)_.*"),
+    names_pattern = "([[:alpha:]]*)_(.*)",
+    names_to = c("name", ".value")
+  ) |> 
+  mutate(
+    data_source = factor(
+      data_source,
+      levels = c(
+        levels(abun_data$stockid_source), 
+        levels(abun_data$escapement_data_source)
+      )
+    )
+  ) |> 
+  arrange(data_source) |> 
+  mutate(colour = fct_inorder(colour)) |> 
+  filter(value > 0) |> 
+  droplevels()
+  
 
 # The time series plot
-(cu_abun_p <- abun_data |> 
-    filter(!CU == "Somass aggregate") |> 
-    pivot_longer(c(catch, escapement)) |> 
+(cu_abun_p <- cu_abun_data |> 
     ggplot(aes(year, value/1000)) +
     facet_wrap(
       ~CU,
@@ -141,8 +313,17 @@ cu_catch <- tribble(
       colour = "grey40"
     ) +
     geom_col(
-      aes(fill = name),
+      aes(
+        group = data_source,
+        fill = colour
+      ),
+      position = "stack",
       colour = "black"
+    ) +
+    scale_fill_identity(
+      name = "Catch (grey) and\nescapement (colour)\ndata sources",
+      guide = "legend",
+      labels = levels(cu_abun_data$data_source)
     ) +
     scale_y_continuous(
       name = "Sockeye abundance (1000s)",
@@ -154,15 +335,11 @@ cu_catch <- tribble(
       limits = c(1908, max(abun_data$year) +2),
       expand = c(0, 0)
     ) +
-    scale_fill_manual(
-      values = c("grey", "black")
-    ) +
     theme(
-      legend.position = "inside",
-      legend.position.inside = c(0.02, 0.98),
-      legend.justification.inside = c(0, 1),
-      legend.background = element_rect(colour = "black", fill = "white"),
-      legend.title = element_blank(),
+      legend.position = "right",
+      # legend.position.inside = c(0.02, 0.98),
+      # legend.justification.inside = c(0, 1),
+      # legend.background = element_rect(colour = "black", fill = alpha("white", 0.7)),
       strip.background = element_rect(colour = "black", fill = "white")
     )
 )
