e2c32cde4201961600c79b47603afa750f76ecfe
Nick Brown <nickawbrown@gmail.com>
2025-02-28 15:50:40 -0800

    working through more realistic simulation based on PT's example

--- 2. code/Stock-recruit data preparation/R/infill_smolt_age_time_series.R
+++ 2. code/Stock-recruit data preparation/R/infill_smolt_age_time_series.R
@@ -332,3 +332,188 @@ lakes_pred_frame |>
   labs(y = "Percent composition") +
   theme(panel.spacing.y = unit(1, "lines"))
 
+
+
+# Patrick Thompson's code -------------------------------------------------
+
+
+library(tidybayes)
+
+#simulate fake data##
+
+
+# Nick messing around to understand sigma_theta
+sigma_theta_p <- function(mu_theta) {
+  data.frame(
+  mu_theta = mu_theta,
+  sigma_theta = seq(0.1, 1.5, by = 0.1)
+) |> 
+  rowwise() |> 
+  mutate(sim_theta = list(plogis(rnorm(1e5, mu_theta, sigma_theta)))) |> 
+  unnest(sim_theta) |> 
+  ggplot(aes(x = sim_theta)) +
+  facet_wrap(~sigma_theta, labeller = label_both) +
+  geom_histogram(binwidth = 0.01) +
+  geom_vline(xintercept = plogis(mu_theta), lty = 2) +
+  coord_cartesian(expand = FALSE)
+}
+# Similar simulations likely useful in understanding the other parameters
+
+
+# Print some plots showing sigma_theta and mu_theta combos
+c(logit(seq(0.15, 0.95, by = 0.1))) |> 
+  map(sigma_theta_p)
+
+
+# Do simulations on realistic values for the three lakes
+
+# Function to simulate some data
+make_sim_data <- function(
+    Y = 30, # years
+    phi = 0.1, # dispersion parameter
+    mu_theta, # logit-transformed smolting rate mean estimate
+    sigma_theta, # interannual smolting rate variation following a normal distribution in logit space
+    mu_M, # logit-transformed overwinter mortality rate
+    sigma_M, # logit-transformed overwinter mortality variation
+    N1_mu, # Mean number of age1 fry prior to smolting
+    N1_phi = 10, # dispersion parameter around number smolting
+    init_N2 # Initial number of age1 holdovers
+) {
+  
+  N1 <- rnbinom(n = Y, mu = N1_mu, size = N1_phi)
+  N2 <- rep(NA, Y)
+  N2[1] <- init_N2
+  
+  theta <- plogis(rnorm(Y, mu_theta, sigma_theta))
+  hist(theta)
+  
+  M <- plogis(rnorm(Y, mu_M, sigma_M)) # Mortality age1 to age2 in lake
+  
+  N_lake <- O1 <- O2 <- rep(NA, Y)
+  for(y in 1:Y){
+    if(y > 1){
+      N2[y] = N1[y-1] * (1 - theta[y-1]) * M[y-1]
+    }
+    N_lake[y] = N1[y] + N2[y]
+    
+    # Outmigrants
+    O1[y] = theta[y] * N1[y]
+    O2[y] = N2[y]
+  }
+  
+  obs_error <- 0.001
+  N_obs <- rnbinom(Y, mu = N_lake, 1/obs_error)
+  
+  A_total <- rbinom(n = Y, size = N_obs, prob = 0.02) # Observed fish outmigrating
+  A1_obs <- rbinom(n = Y, size = A_total, prob = O1 / (O1 + O2)) # Observed Age1 fish outmigrating
+  
+  dat_list <- list(
+    Y = Y, 
+    N_obs = N_obs, 
+    N_lake = N_lake,
+    A1_obs = A1_obs, 
+    A_total = A_total, 
+    obs_error_prior = 1/0.001,
+    mu_theta_prior = 2,
+    sigma_theta_prior = 0.5,
+    mu_M_prior = -1.5,
+    sigma_M_prior = 0.5,
+    N2_init_prior = 300,
+    N2_init_sigma_prior = 0.2
+  )
+  
+  return(dat_list)
+}
+
+
+# Logit transformation
+logit <- function(p) {log(p/(1-p))}
+
+
+# Data for the three lakes
+sim_data <- data.frame(
+    lake = c("Great Central", "Sproat", "Hucuktlis"),
+    mu_theta = logit(c(0.85, 0.9, 0.95)), 
+    sigma_theta = c(0.5, 0.5, 0.9), 
+    mu_M = logit(c(.2, .45, .7)), # haphazard guesses 
+    sigma_M = c(0.4, 0.5, 0.7), 
+    N1_mu = c(7e6, 6e6, 1.5e6), 
+    N1_phi = c(8, 10, 5), # smaller phi = more variable
+    init_N2 = c(0.15*7e6, 0.1*6e6, 0.05*1.5e6)
+) %>%
+  split(.$lake) |> 
+  map(\(x) select(x, -lake)) |> 
+  map(as.vector) |> 
+  map(\(x) do.call(make_sim_data, x))
+  
+
+# Look at lake observed versus total fry
+sim_data |> 
+  map(\(x) plot(x$N_obs ~ x$N_lake))
+
+
+#run stan model##
+model <- stan_model(
+  here(
+    "2. code",
+    "Stock-recruit data preparation",
+    "Stan",
+    "age_prop_beta_PT.stan"
+  )
+)
+fit <- sampling(model, data = dat_list, chains = 4, cores = 4, iter = 1000)
+
+#assess convergence###
+worst_Rhat <- summary(fit)$summary %>% 
+  as.data.frame() %>% 
+  mutate(Rhat = round(Rhat, 3)) %>% 
+  arrange(desc(Rhat))
+
+worst_Rhat %>% 
+  filter(n_eff>3) %>% 
+  ggplot(aes(x = n_eff, y = Rhat))+
+  geom_point()+
+  geom_hline(yintercept = 1.01, lty = 2)+
+  geom_vline(xintercept = 400, lty = 2)
+
+head(worst_Rhat, n = 20)
+
+traceplot(fit, pars = rownames(worst_Rhat)[1:20])
+pairs(fit, pars = c("mu_theta", "mu_M"))
+
+#explore posterior###
+post <- extract(fit)
+
+plot(density(post$mu_M))
+abline(v = mu_M)
+
+plot(density(post$mu_theta))
+abline(v = mu_theta)
+
+plot(density(post$init_N2))
+abline(v = init_N2)
+
+spread_draws(fit, O1[year]) %>% 
+  ggplot(aes(x = year, y = O1)) +
+  stat_pointinterval(.width = c(0.5, 0.8, 0.95))+
+  geom_point(data = data.frame(year = 1:Y, O1 = O1), color = "red")
+
+spread_draws(fit, O2[year]) %>% 
+  ggplot(aes(x = year, y = O2)) +
+  stat_pointinterval(.width = c(0.5, 0.8, 0.95))+
+  geom_point(data = data.frame(year = 1:Y, O2 = O2), color = "red")
+
+spread_draws(fit, theta[year]) %>% 
+  ggplot(aes(x = year, y = theta)) +
+  stat_pointinterval(.width = c(0.5, 0.8, 0.95))+
+  geom_point(data = data.frame(year = 1:Y, theta = theta), color = "red") +
+  scale_y_continuous(
+    limits = c(0, 1),
+    expand = c(0,0)
+  )
+
+spread_draws(fit, M[year]) %>% 
+  ggplot(aes(x = year, y = M)) +
+  stat_pointinterval(.width = c(0.5, 0.8, 0.95))+
+  geom_point(data = data.frame(year = 1:Y, M = M), color = "red")
+ 
