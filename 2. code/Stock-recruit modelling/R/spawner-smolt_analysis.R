c72b437f58b46204740660ace61691e86e4f7e27
Nick Brown <nickawbrown@gmail.com>
2025-03-20 09:19:37 -0700

    implement Bayesian Beverton-Holt model for Somass

--- 2. code/Stock-recruit modelling/R/spawner-smolt_analysis.R
+++ 2. code/Stock-recruit modelling/R/spawner-smolt_analysis.R
@@ -1,7 +1,7 @@
 # Packages ----------------------------------------------------------------
 
 
-pkgs <- c("here", "readxl", "tidyverse", "broom")
+pkgs <- c("here", "readxl", "tidyverse", "broom", "gsl", "rstan")
 #install.packages(pkgs)
 
 
@@ -9,7 +9,8 @@ library(here)
 library(tidyverse); theme_set(theme_bw())
 library(readxl)
 library(broom)
-
+library(gsl)
+library(rstan)
 
 
 # Load pre-smolt abundance and adult spawner data --------------
@@ -54,7 +55,7 @@ spwn <- here(
     # Align hatchery fry releases by brood year
     hatchery_fry_release = lead(hatchery_fry_release, 2)
   ) |> 
-  select(brood_year, cu, S, adult_S, R, fertilized, hatchery_fry_release)
+  select(brood_year, cu, S, adult_S, S_cv, R, fertilized, hatchery_fry_release)
 
 
 # Join spawner data to fry data
@@ -62,6 +63,8 @@ spwn_fry <- left_join(
   fry_abun,
   select(spwn, -R)
 ) |>
+  # Specify that "mu" and "sigma" columns refer to recruits
+  rename_with(\(x) paste0(x, "_R"), .cols = c(mu, sigma)) |> 
   mutate(
     hatchery_fry_release = if_else(
       is.na(hatchery_fry_release), 
@@ -145,6 +148,12 @@ sr_plot_data <- spwn_fry |>
         ),
         linewidth = 0.3,
         alpha = 0.3
+      ) +
+      geom_errorbarh(
+        aes(
+          xmin = value - 1.96*value*S_cv,
+          xmax = value + 1.96*value*S_cv,
+        )
       ) +
       geom_pointrange(
         aes(
@@ -285,7 +294,7 @@ nested_data |>
 
 
 
-# Fit simple Ricker models and examine fits -------------------------------
+# Fit simple models and examine fits -------------------------------
 
 
 # Use the data that were assembled above for plotting
@@ -323,33 +332,68 @@ model_fits <- nested_data |>
         )
       ),
     # Dataframe for predictions
-    newdata = if_else(
-      cu == "Hucuktlis",
-      list(
+    newdata = case_when(
+      cu == "Hucuktlis" ~ list(
         expand_grid(
           S = seq(
             0.01,
-            #min(data$S, na.rm = TRUE),
             max(data$S, na.rm = TRUE), 
             length.out = 100
           ),
           fertilized = factor(c(0, 1))
         )
       ),
-      list(
-        tibble(
+      cu == "Sproat" ~ list(
+        expand_grid(
           S = seq(
             0.01,
-            #min(data$S, na.rm = TRUE),
             max(data$S, na.rm = TRUE), 
             length.out = 100
-          )
+          ),
+          fertilized = factor(0)
+        )
+      ),
+      cu == "Great Central" ~ list(
+        expand_grid(
+          S = seq(
+            0.01,
+            max(data$S, na.rm = TRUE), 
+            length.out = 100
+          ),
+          fertilized = factor(1)
         )
       )
     )
   ) |> 
   mutate(
     ricker_pred = if(cu == "Hucuktlis")
+      list(
+      MASS::mvrnorm(
+        10000, 
+        mu = coef(ricker_model), 
+        Sigma = as.matrix(vcov(ricker_model))
+      ) |> 
+        as_tibble() |> 
+        rename("a0" = 1, "b" = 2, "a1" = 3) |> 
+        expand_grid(newdata) |> 
+        mutate(
+          a1 = a0 + a1,
+          across(matches("^a"), exp),
+          b = -b,
+          pred_y = if_else(
+            fertilized == "0",
+            a0*S*exp(-b*S),
+            a1*S*exp(-b*S)
+          )
+        ) |> 
+        summarize(
+          .by = c(S, fertilized),
+          fit = mean(pred_y),
+          lwr = quantile(pred_y, 0.025),
+          upr = quantile(pred_y, 0.975)
+        )
+      )
+    else 
       list(
         MASS::mvrnorm(
           10000, 
@@ -366,28 +410,6 @@ model_fits <- nested_data |>
           ) |> 
           summarize(
             .by = c(S, fertilized),
-            fit = mean(pred_y),
-            lwr = quantile(pred_y, 0.025),
-            upr = quantile(pred_y, 0.975)
-          )
-      )
-    else
-      list(
-        MASS::mvrnorm(
-          10000, 
-          mu = coef(ricker_model), 
-          Sigma = as.matrix(vcov(ricker_model))
-        ) |> 
-          as_tibble() |> 
-          rename("a" = 1, "b" = 2) |> 
-          expand_grid(newdata) |> 
-          mutate(
-            a = exp(a),
-            b = -b,
-            pred_y = a*S*exp(-b*S)
-          ) |> 
-          summarize(
-            .by = c(S),
             fit = mean(pred_y),
             lwr = quantile(pred_y, 0.025),
             upr = quantile(pred_y, 0.975)
@@ -420,9 +442,8 @@ model_fits <- nested_data |>
 
 
 # Plot the various model fits
-(model_plots <- model_fits |>  
-  filter(cu != "Hucuktlis") %>% 
-  split(.$predictor) |> 
+(model_plots <- model_fits %>%  
+  split(.$cu) |> 
   imap(
     function(set, idx) {
       
@@ -436,33 +457,41 @@ model_fits <- nested_data |>
         aes(
           x = S, 
           y = y,
-          colour = type,
-          fill = type
+          colour = interaction(fltr, type)
         )
       ) +
         facet_grid(
-          response_long ~ cu,
+          response_long ~ predictor,
           scales = "free",
-          switch = "y"
+          switch = "both"
         ) +
-        geom_point(colour = "black") +
+        geom_point(
+          aes(shape = fertilized),
+          colour = "black"
+        ) +
         geom_line(
           data = sub_set$pred,
-          aes(y = fit)
+          aes(
+            y = fit,
+            lty = fertilized
+          )
         ) +
+        scale_shape_manual(values = c(19, 4)) +
         scale_x_continuous(
-          name = idx,
+          name = NULL,
           labels = scales::label_number(),
+          limits = c(0, NA),
           expand = expansion(mult = c(0, 0.05))
         ) +
         scale_y_continuous(
           name = NULL,
-          #limits = c(0, max(sub_set$pred$upr)),
+          limits = c(0, NA),
           expand = expansion(mult = c(0, 0.05))
         ) +
+        labs(title = idx) +
         theme(
           strip.placement = "outside",
-          strip.background.y = element_blank(),
+          strip.background = element_blank(),
           axis.text.x = element_text(angle = 25, hjust = 1)
         )
     }
@@ -478,8 +507,297 @@ model_plots |>
       filename = here(
         "3. outputs",
         "Plots",
-        paste0("Pre-smolts per ", idx, ".png")
+        paste0(idx, "_spawner-smolt_fits.png")
       ),
       dpi = "print"
     )
   )
+
+
+
+# Plot the recruits per spawner relationships
+(model_plots <- model_fits %>%  
+    split(.$cu) |> 
+    imap(
+      function(set, idx) {
+        
+        sub_set <- list(
+          obs = unnest(set, data),
+          pred = unnest(set, pred)
+        )
+        
+        ggplot(
+          data = sub_set$obs,
+          aes(
+            x = S, 
+            y = log(y/S),
+            colour = interaction(fltr, type)
+          )
+        ) +
+          facet_grid(
+            response_long ~ predictor,
+            scales = "free",
+            switch = "both",
+            labeller = labeller(response_long = ~paste0("log(", .x, "/spawner)"))
+          ) +
+          geom_point(
+            aes(shape = fertilized),
+            colour = "black"
+          ) +
+          geom_line(
+            data = sub_set$pred,
+            aes(
+              y = log(fit/S),
+              lty = fertilized
+            )
+          ) +
+          scale_shape_manual(values = c(19, 4)) +
+          scale_x_continuous(
+            name = NULL,
+            limits = c(0, NA),
+            labels = scales::label_number(),
+            expand = expansion(mult = c(0, 0.05))
+          ) +
+          scale_y_continuous(
+            name = NULL,
+            expand = expansion(mult = c(0, 0.05))
+          ) +
+          labs(title = idx) +
+          theme(
+            strip.placement = "outside",
+            strip.background = element_blank(),
+            axis.text.x = element_text(angle = 25, hjust = 1)
+          )
+      }
+    )
+)
+
+  
+
+# Fit Beverton-Holt AR1 models to Somass data using Stan ------------------
+
+
+# Pull out the Hucuktlis data, which require a different model
+somass_sr <- spwn_fry |> 
+  filter(cu != "Hucuktlis") |> 
+  droplevels()
+  
+
+# Make input data for Stan
+make_stan_data_somass <- function(cu_name) {
+  
+  cu_data <- somass_sr |> 
+    filter(
+      cu == cu_name,
+      !if_any(c(mu_R, sigma_R, adult_S, S_cv), is.na),
+      parameter == "BYO" # Use brood year smolt production as recruitment measure
+    ) |> 
+    mutate(
+      S_sd = adult_S * S_cv,
+      mu_S = log(adult_S^2 / sqrt(S_sd^2 + adult_S^2)),
+      sigma_S = sqrt(log(1 + (S_sd^2 / adult_S^2)))
+    )
+  
+  # Number of years in the time series
+  Y <- nrow(cu_data)
+  
+  # Best estimates of annual spawner abundance, in log space
+  S_obs <- cu_data$mu_S
+  
+  # Best estimates of annual smolt recruitment, in log space
+  R_obs <- cu_data$mu_R
+  
+  # Annual standard deviation of spawner abundance, in log space
+  sigma_S_obs <- cu_data$sigma_S
+  
+  # Annual standard deviation of smolt recruitment, in log space
+  sigma_R_obs <- cu_data$sigma_R
+  
+  stan_data <- list(
+    Y = Y,
+    S_obs = S_obs,
+    R_obs = R_obs,
+    sigma_S_obs = sigma_S_obs,
+    sigma_R_obs = sigma_R_obs
+  )
+  
+  return(stan_data)
+  
+}
+
+
+# Save the stan data for each CU
+somass_stan_data <- levels(somass_sr$cu) |> 
+  purrr::set_names() |> 
+  map(make_stan_data_somass)
+
+
+# Function to fit Stan model 
+fit_stan_somass <- function(stan_data, cu) {
+  stan(
+    file = here(
+      "2. code",
+      "Stock-recruit modelling",
+      "Stan",
+      "SR_Bev-Holt_AR1.stan"
+    ), 
+    model_name = cu,
+    data = stan_data, 
+    iter = 3000, 
+    chains = 4, 
+    warmup = 1000
+  )
+}
+
+
+# Fit the models for both CUs
+if(
+  FALSE
+  #TRUE
+) {
+  somass_stan_fits <- somass_stan_data |> 
+    imap(fit_stan_somass)}
+
+
+# Save fitted models as RDS objects (toggle to TRUE to run)
+if(
+  #TRUE
+  FALSE
+) {
+  somass_stan_fits |> 
+    iwalk(
+      \(x, idx) x |> 
+        saveRDS(
+          file = here(
+            "3. outputs",
+            "Stock-recruit modelling",
+            paste("Bayesian Beverton-Holt", idx, "spawner-smolt.rds")
+          )
+        )
+    )
+}
+
+
+# Load fitted models from RDS files (if not already in global environment)
+somass_stan_fits <- if(exists("somass_stan_fits")) {
+  somass_stan_fits
+} else {
+  levels(somass_sr$cu) |>
+    purrr::set_names() |> 
+    map(
+      \(x) list.files(
+        here(
+          "3. outputs",
+          "Stock-recruit modelling"
+        ),
+        pattern = paste("Bayesian Beverton-Holt", x, "spawner-smolt.rds"),
+        full.names = TRUE
+      )
+    ) |> 
+    map(readRDS)
+}
+
+
+# Assess convergence
+worst_Rhat <- somass_stan_fits |> 
+  map(\(x) summary(x)$summary) |>  
+  map(as.data.frame) |> 
+  list_rbind(names_to = "lake") |> 
+  mutate(Rhat = round(Rhat, 3)) |> 
+  arrange(desc(Rhat))
+
+worst_Rhat %>% 
+  filter(n_eff>3) %>% 
+  ggplot(aes(x = n_eff, y = Rhat))+
+  facet_wrap(~lake) +
+  geom_point()+
+  geom_hline(yintercept = 1.01, lty = 2)+
+  geom_vline(xintercept = 400, lty = 2)
+
+head(worst_Rhat, n = 20)
+
+
+# Trace plots
+somass_stan_fits |> 
+  imap(
+    \(x, idx)
+    traceplot(
+      x, 
+      pars = rownames(filter(worst_Rhat, lake == idx))[1:20]
+    ) +
+      ggtitle(label = idx)
+  )
+
+
+# Pair plots
+somass_stan_fits |> 
+  map(\(x) pairs(x, pars = c("alpha", "beta")))
+
+
+# Explore the posterior
+
+# Dataframe with first year in time series for each lake
+# used to properly code years in the Stan outputs
+min_yrs <- somass_sr |> 
+  summarize(
+    .by = cu,
+    min_yr = min(year)
+  )
+
+
+# Posterior values that are not year-specific
+post_interannual <- somass_stan_fits |> 
+  map(extract) |> 
+  # Discard all parameters with year-specific estimates
+  map(\(x) discard(.x = x, .p = \(y) is.matrix(y))) |> 
+  map(\(x) map(x, \(y) as_tibble(y, rownames = "draw"))) |> 
+  map(\(x) list_rbind(x, names_to = "parameter")) |> 
+  list_rbind(names_to = "cu") 
+
+
+# Posterior values that are year-specific
+post_annual <- somass_stan_fits |> 
+  map(extract) |> 
+  # Keep only parameters with year-specific estimates
+  map(\(x) keep(.x = x, .p = \(y) is.matrix(y))) |> 
+  map(\(x) map(x, \(y) as_tibble(y, .name_repair = NULL, rownames = "draw"))) |> 
+  map(\(x) list_rbind(x, names_to = "parameter")) |> 
+  list_rbind(names_to = "cu") |> 
+  pivot_longer(
+    cols = !c(cu, parameter, draw),
+    names_to = "year",
+    values_to = "value",
+    names_transform = \(x) str_extract(x, "\\d+")
+  ) |> 
+  left_join(min_yrs) |> 
+  mutate(year = as.numeric(year) - 1 + min_yr) |> 
+  select(-min_yr)
+
+
+# All posterior values
+posterior_df <- bind_rows(
+  post_annual, 
+  post_interannual
+) 
+
+
+# Plot posterior estimates for spawners and recruits
+posterior_df |> 
+  filter(parameter %in% c("S_true", "R_true")) |> 
+  pivot_wider(
+    names_from = parameter,
+    values_from = value
+  ) |> 
+  ggplot(aes(S_true, R_true, colour = year)) +
+  facet_wrap(~cu) +
+  stat_summary(
+    geom = "linerange",
+    fun.data = mean_cl_boot,
+    orientation = "x"
+  ) +
+  stat_summary(
+    geom = "pointrange",
+    fun.data = mean_cl_boot,
+    orientation = "y"
+  )
+
