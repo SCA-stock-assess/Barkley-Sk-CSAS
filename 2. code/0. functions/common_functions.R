351782e4dfd0037c4fb701804093e5be00288289
Nick Brown <52330932+nickawbrown@users.noreply.github.com>
2025-08-18 15:09:44 -0700

    include Carrie's Sgen functions

--- 2. code/0. functions/common_functions.R
+++ 2. code/0. functions/common_functions.R
@@ -18,6 +18,39 @@ get_Sgen <- function(a, b, int_lower, int_upper, Smsy){
   return(Sgen)
 }
 
+## Sgen Ricker and Beverton -Holt 
+
+
+sGenSolverRicker <- function (loga, b) {
+  # Function to estimate Sgen from loga and b Ricker parameters
+  # Assuming Ricker form: R=S * exp( loga - b * S), and explicit soln for SMSY
+  sMSY <- (1 - gsl::lambert_W0(exp(1 - loga))) / b
+  fn <- function(S){ -sum( dnorm ( log(sMSY) - log(S) - loga + b*S, 0, 1, log = T))}
+  fit <- optimize(f = fn, interval = c(0, sMSY))
+  return(fit$minimum)
+}
+
+
+# or more simply, an explicit solution for Sgen- Ricker:
+sGenDirect <- function(loga, b){
+  sMSY <- ( 1 - gsl::lambert_W0 (exp ( 1 - loga) ) ) / b
+  a <- exp(loga)
+  -1/b*gsl::lambert_W0(-b*sMSY/a)
+}
+
+
+sGenSolverBH <- function (a, b) {
+  # Function to estimate Sgen from a and b BH parameters
+  # Assuming BH form: R = aS/(1 + (a/b) *S)
+  # Assuming approximation for SMSY: b*sqrt(1/a) - b/a
+  sMSY <- b*sqrt(1/a) - b/a
+  fn <- function(S){ -sum( dnorm ( log(sMSY) - log( a*S/ (1+ (a/b) * S) ), 
+                                   0, 1, log = T)) }
+  fit <- optimize(f = fn, interval = c(0, sMSY))
+  return(fit$minimum)
+}
+
+
 
 # Management table rules --------------------------------------------------
 
